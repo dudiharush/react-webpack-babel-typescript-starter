5974c243c5c462adeff1aaab3a1fec6993289589
David Harush <davidharush@gmail.com>
2019-11-06 15:28:54 +0200

    prettier test

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import Adapter from 'enzyme-adapter-react-16';
-import { mount } from 'enzyme';
-import App from './App';
+import React from 'react'
+import renderer from 'react-test-renderer'
+import Adapter from 'enzyme-adapter-react-16'
+import { mount } from 'enzyme'
+import App from './App'
 
 describe('App', () => {
-    it('renders the inner Counter', () => {
-      const wrapper = mount(<App />);
-      expect(wrapper.find('p').text()).toEqual('Webpack + TypeScript + React = ❤️');
-    });
-  });
+  it('renders the inner Counter', () => {
+    const wrapper = mount(<App />)
+    expect(wrapper.find('p').text()).toEqual('Webpack + TypeScript + React = ❤️')
+  })
+})
