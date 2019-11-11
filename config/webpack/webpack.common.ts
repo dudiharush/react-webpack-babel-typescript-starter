d28c2e5c5067b00dd9dd3d4893b63df4254819d0
David Harush <davidharush@gmail.com>
2019-11-11 19:09:24 +0200

    add style-loader to dev mode

--- config/webpack/webpack.common.ts
+++ config/webpack/webpack.common.ts
@@ -1,63 +1,44 @@
-import { join, resolve} from 'path';
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import { CleanWebpackPlugin } from 'clean-webpack-plugin';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { join, resolve } from 'path'
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import { CleanWebpackPlugin } from 'clean-webpack-plugin'
 
 export interface WebpackEnv {
-  mode?: string;
-  debug?: boolean;
+  mode?: string
+  debug?: boolean
 }
 
 export const getCommonConfig = (env: WebpackEnv = {}): webpack.Configuration => {
-  const { mode = 'development', debug = true } = env;
+  const { mode = 'development', debug = true } = env
 
   return {
-  mode: 'none',
-  entry: {
-    index: join(resolve('src'), 'index.tsx'),
-  },
-  output: {
-    pathinfo: debug === true,
-    path: resolve('dist'),
-    filename: '[name].js',
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: 'babel-loader'
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              hmr: mode === 'development',
-            },
-          },
-          'css-loader',
-        ]
-      }
+    mode: 'none',
+    entry: {
+      index: join(resolve('src'), 'index.tsx'),
+    },
+    output: {
+      pathinfo: debug === true,
+      path: resolve('dist'),
+      filename: '[name].js',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        title: 'Babel + TypeScript + React = ❤️',
+        template: 'src/index.html',
+      }),
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Babel + TypeScript + React = ❤️',
-      template: 'src/index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-      ignoreOrder: false
-    })
-  ]
+  }
 }
-};
--- config/webpack/webpack.dev.ts
+++ config/webpack/webpack.dev.ts
@@ -1,16 +1,28 @@
-import merge from 'webpack-merge';
-import { getCommonConfig } from './webpack.common';
+import merge from 'webpack-merge'
+import { getCommonConfig } from './webpack.common'
+import webpack from 'webpack'
 
-module.exports = merge(getCommonConfig(), {
-   mode: 'development',
-   devtool: 'inline-source-map',
-   devServer: {
-     contentBase: '/dist',
-     port: 8080,
-     open: true,
-     hot: true,
-     compress: true,
-     stats: 'errors-only',
-     overlay: true,
-   }
-});
+const devConfig: webpack.Configuration = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: '/dist',
+    port: 8080,
+    open: true,
+    hot: true,
+    compress: true,
+    stats: 'errors-only',
+    overlay: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+}
+
+export default merge(getCommonConfig(), devConfig)
