c47e36f4705aa70713026149cc39aa701eef5597
David Harush <davidharush@gmail.com>
2020-07-14 11:48:50 +0300

    fix webpack config testing

--- config/webpack/webpack.testing.ts
+++ config/webpack/webpack.testing.ts
@@ -2,4 +2,4 @@ import { getDevConfig } from './webpack.dev'
 import { getCommonConfig } from './webpack.common'
 import merge from 'webpack-merge'
 
-export default merge(getDevConfig(true))
+export default merge(getCommonConfig(), getDevConfig(true))
