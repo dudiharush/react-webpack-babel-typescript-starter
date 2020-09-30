31b749e24dab4d2d0761ae3e1f3aad3cb2c8ea2a
David Harush <davidharush@gmail.com>
2020-09-30 15:08:00 +0300

    'cypress'

--- config/jest.config.js
+++ config/jest.config.js
@@ -1,9 +1,15 @@
 module.exports = {
   rootDir: '../',
   roots: ['<rootDir>/src/'],
+  coveragePathIgnorePatterns: ['<rootDir>/cypress/'],
   transform: {
-    '^.+\\.tsx?$': 'babel-jest',
+      '^.+\\.tsx?$': 'babel-jest',
   },
   setupFiles: ['<rootDir>/config/jest.setup.ts'],
   setupFilesAfterEnv: ['<rootDir>/config/testing-library.setup.ts'],
-}
+  moduleNameMapper: {
+      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+          '<rootDir>/__mocks__/jest/fileMock.js',
+      '\\.(css|less)$': '<rootDir>/__mocks__/jest/styleMock.js',
+  },
+};
