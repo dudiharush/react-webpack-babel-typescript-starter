a6c77dfd657a7b607a203f0d262842598c2b4218
David Harush <davidharush@gmail.com>
2021-06-13 17:08:57 +0300

    fix storybook build via webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,6 @@
 const path = require('path');
 module.exports = {
+    core: { builder: 'webpack5' },
     stories: ['../src/**/*.stories.@(tsx)'],
     addons: ['@storybook/addon-essentials'],
 // Export a function. Accept the base config as the only param.
