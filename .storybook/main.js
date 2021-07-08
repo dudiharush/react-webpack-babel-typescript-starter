625fa9670cd0d968aa59d7c959b704d334ef89aa
David Harush <davidharush@gmail.com>
2021-07-08 09:09:47 +0300

    update Typescript and Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,32 +1,16 @@
 const path = require('path');
+
 module.exports = {
     core: { builder: 'webpack5' },
     stories: ['../src/**/*.stories.@(tsx)'],
     addons: ['@storybook/addon-essentials'],
+    reactOptions: {
+      fastRefresh: true,
+      strictMode: true,
+    },
 // Export a function. Accept the base config as the only param.
   webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    // Make whatever fine-grained changes you need
-    config.module.rules.push({
-        test: /\.(ts|tsx|js|jsx)$/,
-        exclude: /node_modules/,
-        include: /src/,
-        use: [
-          {
-            loader: require.resolve('babel-loader'),
-          },
-          // Optional
-          // {
-          //   loader: require.resolve('react-docgen-typescript-loader'),
-          // },
-        ],
-    });
-
-    config.resolve.extensions.push('.ts', '.tsx');
-
+   
     // Return the altered config
     return config;
   }
